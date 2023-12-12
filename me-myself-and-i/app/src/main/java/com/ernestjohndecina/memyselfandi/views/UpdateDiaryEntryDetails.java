f6a8ca78324beeee3c979e9322d5b730a500209b
ErnestDecina <ernestjohn007@gmail.com>
2023-12-12 21:40:49 +0000

    Working delete diary entry post

---
+++ me-myself-and-i/app/src/main/java/com/ernestjohndecina/memyselfandi/views/UpdateDiaryEntryDetails.java
@@ -0,0 +1,16 @@
+package com.ernestjohndecina.memyselfandi.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ernestjohndecina.memyselfandi.R;
+
+public class UpdateDiaryEntryDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_diary_entry_details);
+    }
+}
